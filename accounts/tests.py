b8eb6e7b21e1cb24401481247a9b9f747fd55495
Nicole Harris <n.harris@kabucreative.com.au>
2014-02-12 19:15:16 +1100

    Added unit test for login page

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,12 @@
-# test what happens from the code's point of view
-
+from django.contrib.auth.views import login
+from django.core.urlresolvers import reverse
 from django.test import TestCase
+
+class LoginPageTest(TestCase):
+
+    def test_login_page_exists(self):
+        response = self.client.get('/', follow=True)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, 'id_username')
+        self.assertContains(response, 'id_password')
