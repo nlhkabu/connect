c7510d46cb3c229766363d9bcf72df26ba7f5015
Nicole Harris <n.harris@kabucreative.com.au>
2014-02-26 08:11:07 +1100

    Added validation to invitation form - email addresses must be unique

--- moderation/forms.py
+++ moderation/forms.py
@@ -1,9 +1,23 @@
 from django import forms
+from django.contrib.auth.models import User
 
 
 class InviteMemberForm(forms.Form):
     """
-    Form for moderator to invite a new member
+    Form for moderator to invite a new member.
     """
-    #first_name = forms.CharField()
     email = forms.EmailField()
+
+    def clean(self):
+        """
+        Make sure email is not already in the system.
+        """
+        email = self.cleaned_data['email']
+        user_emails = [user.email for user in User.objects.all() if user.email]
+
+        if email in user_emails:
+            raise forms.ValidationError("Sorry, this email address is already registered")
+
+        return self.cleaned_data
+
+
