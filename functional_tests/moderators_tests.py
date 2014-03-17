b907155299cdb5615d63880217913c9ec5f533ee
Nicole Harris <n.harris@kabucreative.com.au>
2014-03-18 07:58:42 +1100

    Added content to functional test

--- functional_tests/moderators_tests.py
+++ functional_tests/moderators_tests.py
@@ -15,3 +15,23 @@ from django.contrib.auth.models import User
 
 # She also sees a list of the user's she has already invited to the system,
 # but who have not activated their account.
+
+# She enters the details of her friend who she wants to invite:
+# Lily
+# Black
+# lily@gmail.com
+
+# When she submits the form, Lily now appears in her 'pending users' list.
+# Lily's listing also includes the date and time Janine invited her
+# and the options to resend or revoke the invitation.
+
+# Janine tries to invite someone without a first name
+# Janine tries to invite someone without a last name
+# Janine tries to invite someone without an email address
+# Janine tries to invite someone with an email address that is already registered.
+
+# Janine tries to reinvite someone
+# Janine revokes an invitation without a comment
+# Janine revokes an invation with a comment
+
+# Janine views the moderation logs
