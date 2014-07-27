cccaf2e97c232b6420b59b2342c288e5820979bc
Nicole Harris <n.harris@kabucreative.com.au>
2014-07-27 16:11:40 +1000

    Fixed AppRegistryNotReady exception

--- accounts/utils.py
+++ accounts/utils.py
@@ -1,11 +1,11 @@
-from django.conf import settings
-
-User = settings.AUTH_USER_MODEL
+from django.contrib.auth import get_user_model
 
 def create_inactive_user(email, first_name, last_name):
     """
     Create inactive user with basic details
     """
+    User = get_user_model()
+
     user = User.objects.create_user(email)
     user.is_active = False
     user.first_name = first_name
