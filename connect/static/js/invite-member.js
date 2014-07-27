b48430169773ea22d1da071bf3e42ae7828fdc98
Nicole Harris <n.harris@kabucreative.com.au>
2014-07-27 18:38:26 +1000

    Added TODOs

--- connect/static/js/invite-member.js
+++ connect/static/js/invite-member.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
 
         user = $(this).data('user');
         email = $(this).data('email');
-        $('.reinvite-member-form').show();
+        $('.reinvite-member-form').show(); // TODO: change this to toggle a modal
         $('.reinvite-member-form #id_user_id').val(user);
         $('.reinvite-member-form #id_email').val(email);
     });
@@ -17,7 +17,7 @@ $(document).ready(function(){
         e.preventDefault();
 
         user = $(this).data('user');
-        $('.revoke-invitation-form').show();
+        $('.revoke-invitation-form').show(); // TODO: change this to toggle a modal
         $('.revoke-invitation-form #id_user_id').val(user);
     });
 });
