e74432e1c211808833055bed13f99169a3f539ec
Nicole Harris <n.harris@kabucreative.com.au>
2014-09-22 18:33:36 +1000

    Fixed site processor to not fail if no SiteConfig is available

--- connect_config/context_processors.py
+++ connect_config/context_processors.py
@@ -7,12 +7,16 @@ def site_processor(request):
     Make site details available to all templates.
     """
     site = get_current_site(request)
-    site_config = SiteConfig.objects.get(site=site)
 
-    site.logo = site_config.logo
-    site.tagline = site_config.tagline
-    site.email = site_config.email
-    site.email_header = site_config.email_header
+    try:
+        site_config = SiteConfig.objects.get(site=site)
+        site.logo = site_config.logo
+        site.tagline = site_config.tagline
+        site.email = site_config.email
+        site.email_header = site_config.email_header
+    except:
+        pass
 
     return {'site': site }
+
 
