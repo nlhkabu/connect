c8adcafca92b5267c21a5042125259617cb1eeb5
Nicole Harris <n.harris@kabucreative.com.au>
2014-10-09 12:15:53 +1100

    Unregister permissions in admin

--- moderation/admin.py
+++ moderation/admin.py
@@ -1,8 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models import Permission
 
 from .models import ModerationLogMsg
 
-admin.site.register(Permission)
 admin.site.register(ModerationLogMsg)
-#~admin.site.register(ModerationLogMsg, ModerationLogMsgAdmin)
