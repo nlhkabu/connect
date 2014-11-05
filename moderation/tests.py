f88c98964a70497e84eeb21d4dfc24e28f0869a8
Nicole Harris <n.harris@kabucreative.com.au>
2014-11-05 17:47:24 +1100

    Test get_date_limits() with local timezone

--- moderation/tests.py
+++ moderation/tests.py
@@ -111,7 +111,19 @@ class LogMessageTest(TestCase):
         self.assertEqual(start, expected_start)
         self.assertEqual(end, expected_end)
 
-    #~def test_date_limits_passing_non_UTC_timezone(self):
+    def test_date_limits_passing_non_UTC_timezone(self):
+        local_tz = timezone.get_current_timezone() # TODO: Get user's preferred timezone
+
+        day_1_UTC = datetime.datetime(2011, 8, 15, 8, 15, 12, 0, pytz.UTC)
+        day_1_local = day_1_UTC.astimezone(local_tz)
+
+        expected_start = datetime.datetime(2011, 8, 15, 0, 0, 0, 0, pytz.UTC)
+        expected_end = datetime.datetime(2011, 8, 15, 23, 59, 59, 999999, pytz.UTC)
+
+        start, end = get_date_limits(day_1_local)
+
+        self.assertEqual(start, expected_start)
+        self.assertEqual(end, expected_end)
 
 
 # Urls.py and views.py
