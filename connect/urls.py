c6cfb6a052f940f95bbe87d87428576cf13deefe
Nicole Harris <n.harris@kabucreative.com.au>
2014-12-04 20:02:36 +1100

    Cleanup and translate root URLs

--- connect/urls.py
+++ connect/urls.py
@@ -1,4 +1,6 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.flatpages import urls as flatpages_urls
+from django.utils.translation import ugettext_lazy as _
 
 #TODO: Remove for production
 from django.conf.urls.static import static
@@ -7,11 +9,17 @@ from django.conf import settings
 from django.contrib import admin
 admin.autodiscover()
 
+from accounts import urls as accounts_urls
+from moderation import urls as moderation_urls
+from discover import urls as discover_urls
+from discover.views import dashboard
+
+
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'discover.views.dashboard', name='dashboard'),
-    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
-    url(r'^moderation/', include('moderation.urls', namespace='moderation')),
-    url(r'^dashboard/', include('discover.urls', namespace='discover')),
-    url(r'^pages/', include('django.contrib.flatpages.urls')),
+    url(_(r'^admin/'), include(admin.site.urls)),
+    url(r'^$', dashboard, name='dashboard'),
+    url(_(r'^accounts/'), include(accounts_urls, namespace='accounts')),
+    url(_(r'^moderation/'), include(moderation_urls, namespace='moderation')),
+    url(_(r'^dashboard/'), include(discover_urls, namespace='discover')),
+    url(_(r'^pages/'), include(flatpages_urls)),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) #TODO: Remove for production
