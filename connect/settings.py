6e42ff84a72f7a169d8acae764b4ff8eb83658af
Nicole Harris <n.harris@kabucreative.com.au>
2015-01-09 16:13:46 +1100

    Update email settings

--- connect/settings.py
+++ connect/settings.py
@@ -163,10 +163,10 @@ SITE_URL = 'http://localhost:8000' #TODO: change for production
 # Set 'from' email address for system emails
 
 EMAIL_HOST = os.environ['CONNECT_EMAIL_HOST']
-EMAIL_PORT = os.environ['CONNECT_EMAIL_PORT']
+EMAIL_PORT = int(os.environ['CONNECT_EMAIL_PORT'])
 EMAIL_HOST_USER = os.environ['CONNECT_EMAIL_HOST_USER']
 EMAIL_HOST_PASSWORD = os.environ['CONNECT_EMAIL_HOST_PASSWORD']
-EMAIL_USE_TLS = os.environ['CONNECT_EMAIL_USE_TLS']
+EMAIL_USE_TLS = os.environ['CONNECT_EMAIL_USE_TLS'] == "True"
 
 
 AUTH_USER_MODEL = 'accounts.CustomUser'
