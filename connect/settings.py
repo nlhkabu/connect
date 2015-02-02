71554c3474c17abceb08148d956dd37c7beb3c81
Nicole Harris <n.harris@kabucreative.com.au>
2015-02-02 16:44:18 +1100

    Fix static path

--- connect/settings.py
+++ connect/settings.py
@@ -134,7 +134,7 @@ LOCALE_PATHS = (
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = '/connect/static/'
+STATIC_ROOT = 'connect/static/'
 STATIC_URL = '/static/'
 
 # Additional locations of static files
