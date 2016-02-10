87cfac55b14083fdb8e346b9db1a95bb0f63881a
Nicole Harris <n.harris@kabucreative.com>
2016-02-10 22:26:08 +0000

    Reconfigure SiteConfigFactory to use JPG - removes pillow's libjpeg-dev dependency

--- connect/config/factories.py
+++ connect/config/factories.py
@@ -18,6 +18,7 @@ class SiteConfigFactory(factory.django.DjangoModelFactory):
         model = SiteConfig
 
     site = factory.SubFactory(Site)
+    logo = factory.django.ImageField(filename='my_log.png', format='PNG')
     email = factory.Sequence(lambda n: "site.email%s@test.test" % n)
     tagline = 'A tagline'
-    email_header = factory.django.ImageField(filename='my_image.png')
+    email_header = factory.django.ImageField(filename='my_image.png', format='PNG')
